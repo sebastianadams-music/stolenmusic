657dfe17db89533f30b7cc1efaa03cbcd5aaee6d
sebastianadams-music <72510504+sebastianadams-music@users.noreply.github.com>
2023-05-04 19:51:07 +0200

    unknown changes to livechat.js and youtube_anon.js

--- s/youtube_anon.js
+++ s/youtube_anon.js
@@ -1,4 +1,6 @@
- // 2. This code loads the IFrame Player API code asynchronously.
+// version for anonymous version submitted to competitions. ignore otherwise.
+
+// 2. This code loads the IFrame Player API code asynchronously.
  var tag = document.createElement('script');
 
  tag.src = "https://www.youtube.com/iframe_api";
